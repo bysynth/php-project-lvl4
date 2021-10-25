d4733bf96ae5d5e9f5755e2cc5e28b8ad306b0d8
VK <belsynth@gmail.com>
2021-10-25 13:00:16 +0300

    Добавляет локализацию флеш-сообщений для экшенов TaskStatusController

---
+++ resources/lang/en/flash.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'task_statuses' => [
+        'store' => [
+            'success' => 'Status successfully created'
+        ],
+        'update' => [
+            'success' => 'Status changed successfully'
+        ],
+        'destroy' => [
+            'success' => 'Status deleted successfully',
+            'failed' => 'Failed to delete status'
+        ]
+    ]
+];
