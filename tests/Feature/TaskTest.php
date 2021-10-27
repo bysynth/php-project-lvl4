ea350b599934f9349f3561eefaf747c36c1a824b
VK <belsynth@gmail.com>
2021-10-27 15:29:03 +0300

    Добавляет тесты для TaskController

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use App\Models\User;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    private User $user;
+    private Task $task;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->task = Task::factory()->create();
+    }
+
+    public function testIndex(): void
+    {
+        $response = $this->get(route('tasks.index'));
+        $response->assertOk();
+    }
+
+    public function testCreate(): void
+    {
+        $response = $this->actingAs($this->user)
+            ->get(route('tasks.create'));
+        $response->assertOk();
+    }
+
+    public function testStore(): void
+    {
+        $data = Task::factory()
+            ->for($this->user, 'creator')
+            ->make()
+            ->toArray();
+
+        $response = $this->actingAs($this->user)
+            ->post(route('tasks.store', $data));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+        $this->assertDatabaseHas('tasks', $data);
+    }
+
+    public function testStoreWithExistingTaskName(): void
+    {
+        $data = Task::factory()
+            ->for($this->user, 'creator')
+            ->make(['name' => $this->task->name])
+            ->toArray();
+        $response = $this->actingAs($this->user)
+            ->post(route('tasks.store', $data));
+        $response->assertSessionHasErrors('name');
+        $response->assertRedirect();
+        $this->assertDatabaseMissing('tasks', $data);
+    }
+
+    public function testShow():void
+    {
+        $response = $this->actingAs($this->user)
+            ->get(route('tasks.show', $this->task));
+        $response->assertOk();
+    }
+
+    public function testEdit(): void
+    {
+        $response = $this->actingAs($this->user)
+            ->get(route('tasks.edit', $this->task));
+        $response->assertOk();
+    }
+
+    public function testUpdate(): void
+    {
+        $data = Task::factory()
+            ->make(['created_by_id' => $this->task->creator->id])
+            ->toArray();
+
+        $response = $this->actingAs($this->user)
+            ->patch(route('tasks.update', $this->task), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+        $this->assertDatabaseHas('tasks', $data);
+    }
+
+    public function testUpdateWithExistingTaskName(): void
+    {
+        $anotherTask = Task::factory()->create();
+        $existingName = $anotherTask->name;
+        $data = Task::factory()
+            ->make([
+                'name' => $existingName,
+                'created_by_id' => $this->task->creator->id,
+            ])
+            ->toArray();
+
+        $response = $this->actingAs($this->user)
+            ->patch(route('tasks.update', $this->task), $data);
+        $response->assertSessionHasErrors('name');
+        $response->assertRedirect();
+    }
+
+    public function testDestroy(): void
+    {
+        $existUserTask = Task::factory()->for($this->user, 'creator')->create();
+        $response = $this->actingAs($this->user)
+            ->delete(route('tasks.destroy', $existUserTask));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+        $this->assertDeleted($existUserTask);
+    }
+
+    public function testDestroyTaskThatNotBelongToUser(): void
+    {
+        $response = $this->actingAs($this->user)
+            ->delete(route('tasks.destroy', $this->task));
+        $response->assertForbidden();
+    }
+}
