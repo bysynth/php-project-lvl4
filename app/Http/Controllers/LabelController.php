dc7ef871f4b863513f7aacc86b406af2a8012101
VK <belsynth@gmail.com>
2021-10-27 20:51:55 +0300

    Добавляет LabelController

---
+++ app/Http/Controllers/LabelController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Label;
+use Illuminate\Http\Request;
+
+class LabelController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function edit(Label $label)
+    {
+        //
+    }
+
+    public function update(Request $request, Label $label)
+    {
+        //
+    }
+
+    public function destroy(Label $label)
+    {
+        //
+    }
+}
